1d36c2645e92681d6a13ca8ad026f38d325df06c
Mukesh Kumar <mukeshk@sdt.(none)>
2015-08-10 10:14:23 +0530

    new changes

---
+++ sites/all/themes/newsflash/js/custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function(){
+  jQuery(' #header #rightbanner .content #menu img').click(function(){
+  	jQuery(' #suckerfishmenu').toggle();
+  });
+function debouncer( func , timeout ) {
+   var timeoutID , timeout = timeout || 200;
+   return function () {
+      var scope = this , args = arguments;
+      clearTimeout( timeoutID );
+      timeoutID = setTimeout( function () {
+          func.apply( scope , Array.prototype.slice.call( args ) );
+      } , timeout );
+   }
+}
+
+
+jQuery( window ).resize( debouncer( function ( e ) {
+    location.reload();
+} ) );
+  	
+
+});
