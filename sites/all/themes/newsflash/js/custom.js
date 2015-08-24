1b07291c84ecaddefd8dd72e5a70f62fd5668778
Mukesh Kumar <mukeshk@sdt.(none)>
2015-08-24 17:38:00 +0530

    added css and js for humbergermenu

--- sites/all/themes/newsflash/js/custom.js
+++ sites/all/themes/newsflash/js/custom.js
@@ -1,22 +1,22 @@
 jQuery(document).ready(function(){
-  jQuery(' #header #rightbanner .content #menu img').click(function(){
+  jQuery(' #header #rightbanner button#menu').click(function(){
   	jQuery(' #suckerfishmenu').toggle();
   });
-/*function debouncer( func , timeout ) {
-   var timeoutID , timeout = timeout || 200;
-   return function () {
+function debouncer( func , timeout ) {
+    var timeoutID , timeout = timeout || 200;
+  return function () {
       var scope = this , args = arguments;
       clearTimeout( timeoutID );
-      timeoutID = setTimeout( function () {
+     timeoutID = setTimeout( function () {
           func.apply( scope , Array.prototype.slice.call( args ) );
-      } , timeout );
-   }
+     } , timeout );
+  }
 }
 
 
 jQuery( window ).resize( debouncer( function ( e ) {
     location.reload();
 } ) );
-  	*/
+  	
 
 });
